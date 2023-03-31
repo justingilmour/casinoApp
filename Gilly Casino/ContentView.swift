cbde1926668cacd14ea47647c361606d56b042e0
Justin Gilmour <justin.gilmour@Justin-Gilmour-CX4NQGF9YW.local>
2023-03-30 23:16:06 -0600

    First push of Swift project to main branch.

---
+++ Gilly Casino/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Gilly Casino
+//
+//  Created by Justin Gilmour on 3/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gilly Casino/Gilly_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gilly_CasinoApp.swift
+//  Gilly Casino
+//
+//  Created by Justin Gilmour on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Gilly_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
